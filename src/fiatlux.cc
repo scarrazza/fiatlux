7b8eb548aa3f3269e71ee264632f51c7d2e2df63
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-10-17 18:18:19 +0200

    initial import

---
+++ src/fiatlux.cc
@@ -0,0 +1,13 @@
+//
+// FiatLux 2016
+//
+// Authors: Stefano Carrazza: stefano.carrazza@cern.ch
+
+#include "fiatlux/fiatlux.h"
+
+namespace fiatlux
+{
+  FiatLux::FiatLux()
+  {
+  }
+}
---
+++ src/fiatlux/fiatlux.h
@@ -0,0 +1,15 @@
+//
+// FiatLux 2016
+//
+// Authors: Stefano Carrazza: stefano.carrazza@cern.ch
+
+#pragma once
+
+namespace fiatlux
+{
+  class FiatLux
+  {
+  public:
+    FiatLux();
+  };
+}
