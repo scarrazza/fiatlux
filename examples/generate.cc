1c37e1a24f7cdda7ba59b83c66847017e4c0a5e2
Stefano Carrazza <stefano.carrazza@cern.ch>
2016-10-17 18:30:22 +0200

    adding example code

---
+++ examples/generate.cc
@@ -0,0 +1,14 @@
+//
+// FiatLux 2016
+//
+// Authors: Stefano Carrazza: stefano.carrazza@cern.ch
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  cout << "Hello" << endl;
+
+  return 0;
+}
