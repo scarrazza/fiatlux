f25aebb4700d095733a0a6af2f5562ad8d85ff56
Stefano Carrazza <stefano.carrazza@cern.ch>
2022-10-28 22:25:55 +0200

    adding long description

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-import sys
+import sys, os
 
 try:
     from skbuild import setup
@@ -12,6 +12,11 @@ except ImportError:
 
 from setuptools import find_packages
 
+# load long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
+    long_description = f.read()
+
 setup(
     name="fiatlux",
     version="0.1.1",
@@ -24,4 +29,6 @@ setup(
     cmake_args=['-DPYTHON_ONLY:BOOL=ON'],
     extras_require={"test": ["pytest"]},
     python_requires=">=3.6",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 )
