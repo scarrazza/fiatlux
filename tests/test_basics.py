9553abd4b1247182b53abfa1ab8e6c44eecfbb92
Alessandro Candido <candido.ale@gmail.com>
2024-03-04 18:10:09 +0100

    test: Lower approximate comparison relative tolerance

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -17,6 +17,9 @@ def fl(x, q):
 def f2lo(x, q):
     return 0.0
 
+def approx(x):
+    return pytest.approx(x, rel=1e-14)
+
 
 def test_loading():
     lux = fiatlux.FiatLux("examples/runcard.yml")
@@ -24,7 +27,8 @@ def test_loading():
     lux.PlugStructureFunctions(f2, fl, f2lo)
     lux.InsertInelasticSplitQ([4.18, 1e100])
     pht = lux.EvaluatePhoton(0.1, 1000)
-    assert pytest.approx(pht.elastic) == 0.0601621248523242
-    assert pytest.approx(pht.inelastic_pf) == 0.01927555943602428
-    assert pytest.approx(pht.msbar_pf) == 0.0
-    assert pytest.approx(pht.total) == 0.07943768428834848
+
+    assert approx(pht.elastic) == 0.0601621248523242
+    assert approx(pht.inelastic_pf) == 0.01927555943602428
+    assert approx(pht.msbar_pf) == 0.0
+    assert approx(pht.total) == 0.07943768428834848
