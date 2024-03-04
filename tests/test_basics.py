f5c0371bb838c9cafdd98d2d3efab138d74318b2
Alessandro Candido <candido.ale@gmail.com>
2024-03-04 17:59:50 +0100

    test: Handle float comparison with Pytest

--- tests/test_basics.py
+++ tests/test_basics.py
@@ -1,4 +1,5 @@
 import fiatlux
+import pytest
 
 
 def a(q):
@@ -6,15 +7,15 @@ def a(q):
 
 
 def f2(x, q):
-    return 0.
+    return 0.0
 
 
 def fl(x, q):
-    return 0.
+    return 0.0
 
 
 def f2lo(x, q):
-    return 0.
+    return 0.0
 
 
 def test_loading():
@@ -23,7 +24,7 @@ def test_loading():
     lux.PlugStructureFunctions(f2, fl, f2lo)
     lux.InsertInelasticSplitQ([4.18, 1e100])
     pht = lux.EvaluatePhoton(0.1, 1000)
-    assert pht.elastic == 0.0601621248523242
-    assert pht.inelastic_pf == 0.01927555943602428
-    assert pht.msbar_pf == 0.0
-    assert pht.total == 0.07943768428834848
+    assert pytest.approx(pht.elastic) == 0.0601621248523242
+    assert pytest.approx(pht.inelastic_pf) == 0.01927555943602428
+    assert pytest.approx(pht.msbar_pf) == 0.0
+    assert pytest.approx(pht.total) == 0.07943768428834848
